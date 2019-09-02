0caf44ad04cc76e6a076b1b3a37d6bee83833c2d
kimjongmo <kfmd1008@naver.com>
2019-09-02 22:58:31 +0900

    spring mvc 프로젝트 세팅

---
+++ web-mvc/src/main/java/com/spring/mvc/chap07/quickstart/HelloController.java
@@ -0,0 +1,15 @@
+package com.spring.mvc.chap07.quickstart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello.do")
+    public String hello(Model model) {
+        model.addAttribute("greeting", "안녕하세요");
+        return "hello";
+    }
+}
